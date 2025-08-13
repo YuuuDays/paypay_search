69af97e84f53158d101d747547a9b5ed4262def8
hikida <ponponda0103@gmail.com>
2025-08-13 17:29:33 +0900

    mainの呼び出し方の変更

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from src.monthly_expenses import monthly_expenses
 
 # コンビニ
 # combine_count()
-
+csv_path = "./csv_storage/202508.csv"
 #月支払い分類
-monthly_expenses()
+result = monthly_expenses(csv_path)
+print(result)
